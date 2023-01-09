be07f19db02d1585374252d85b12372331fb7bee
saulo <saulodavi2001@gmail.com>
2023-01-09 14:30:28 -0300

    starting insert user from gitHub login

--- src/controllers/authentication-controller.ts
+++ src/controllers/authentication-controller.ts
@@ -1,6 +1,10 @@
 import authenticationService, { SignInParams } from "@/services/authentication-service";
 import { Request, Response } from "express";
 import httpStatus from "http-status";
+import dotenv from "dotenv";
+import axios from "axios";
+dotenv.config();
+
 export async function singInPost(req: Request, res: Response) {
   const { email, password } = req.body as SignInParams;
 
@@ -10,5 +14,56 @@ export async function singInPost(req: Request, res: Response) {
     return res.status(httpStatus.OK).send(result);
   } catch (error) {
     return res.status(httpStatus.UNAUTHORIZED).send({});
+  }
+}
+
+export async function singInByGitHub(req: Request, res: Response) {
+  const { code } = req.params;
+  try {
+    const response = await exchangeCodedForAccessToken(code);
+    console.log("res.send token :", response);
+    return res.send(response);
+  } catch (error) {
+    console.log("error :", error);
+  } 
+}
+
+export async function exchangeCodedForAccessToken(code: string) {
+  const CLIENT_ID = process.env.CLIENT_ID;
+  const CLIENT_SECRET = process.env.CLIENT_SECRET;
+  const body = {
+    code,
+    grant_type: "authorization_code",
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET
+  };
+
+  const { data } = await axios.post("https://github.com/login/oauth/access_token", body, {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+  const urlParams = new URLSearchParams(data);
+  const params = Object.fromEntries(urlParams);
+
+  return params;
+}
+
+export async function getUserDataFromGitHub(req: Request, res: Response) {
+  const { token } = req.params;
+  console.log("token :", token);
+
+  try{
+    const response = await axios.get("https://api.github.com/user", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    console.log("userData :", response.data);
+    return res.status(httpStatus.OK).send(response.data);
+  } catch(err) {
+    console.log("err catch getUSerDataFromGitHub :", err);
+    return res.status(httpStatus.UNAUTHORIZED);
   }
 }
--- src/routers/authentication-router.ts
+++ src/routers/authentication-router.ts
@@ -1,4 +1,4 @@
-import { singInPost } from "@/controllers";
+import { getUserDataFromGitHub, singInByGitHub, singInPost } from "@/controllers";
 import { validateBody } from "@/middlewares";
 import { signInSchema } from "@/schemas";
 import { Router } from "express";
@@ -6,5 +6,7 @@ import { Router } from "express";
 const authenticationRouter = Router();
 
 authenticationRouter.post("/sign-in", validateBody(signInSchema), singInPost);
+authenticationRouter.get("/github/:code", singInByGitHub);
+authenticationRouter.get("/github/signIn/:token", getUserDataFromGitHub);
 
 export { authenticationRouter };
